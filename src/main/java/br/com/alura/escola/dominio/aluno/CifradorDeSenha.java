932dbb28ae55e7827617f62721bc05e47c832ea2
Jonas César Silva <jonascesar.silva@dxc.com>
2022-12-05 14:33:09 -0300

    Implementação do CifradorDeSenhaComMD5

--- src/main/java/br/com/alura/escola/dominio/aluno/CifradorDeSenha.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/CifradorDeSenha.java
@@ -1,5 +1,5 @@
 package br.com.alura.escola.dominio.aluno;
-
+//SERVIÇO DE DOMÍNIO DE CIFRAR SENHA
 //Essa interface fica na camada de domínio pois representa o conceito de cifrar uma senha (conceito pertencente ao domínio).
 //Agora como esse processo de cifra vai acontecer diz respeito à camada de infraestrutura (implementação do serviço de cifrar senha).
 public interface CifradorDeSenha {
